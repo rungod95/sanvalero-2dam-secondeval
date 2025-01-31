dd5646e29b22820a9010daf2e2e071d53acdd5e8
Jplanas <jabierplanos@gmail.com>
2025-01-31 11:31:36 +0100

    Added and implemented JWt Token configuration

---
+++ api/src/main/java/com/svalero/films/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.svalero.films.config;
+
+import com.svalero.films.security.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("api/auth/login", "api/auth/register").permitAll()
+                        .requestMatchers("/films", "/films/{id}", "/directors", "/directors/{id}").permitAll() // Acceso libre a GET, POST y PUT
+                        .requestMatchers(HttpMethod.DELETE, "/films/{id}", "/directors/{id}").authenticated() // Requiere JWT solo en DELETE
+                        .anyRequest().authenticated()
+                )
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+            return authenticationConfiguration.getAuthenticationManager();
+        }
+    }

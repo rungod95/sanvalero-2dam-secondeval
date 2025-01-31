001333eb71a76664cd9bbfad3dd6d715b68adab6
JMuniesaSV <a27905@svalero.com>
2025-01-31 13:11:15 +0100

    Add loggin system and fix local commit conflict

--- api/src/main/java/com/svalero/films/controller/FilmController.java
+++ api/src/main/java/com/svalero/films/controller/FilmController.java
@@ -4,7 +4,7 @@ import com.svalero.films.domain.Film;
 import com.svalero.films.exception.InvalidDataException;
 import com.svalero.films.exception.ResourceNotFoundException;
 import com.svalero.films.service.FilmService;
-import org.apache.coyote.Response;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/films")
@@ -20,11 +21,15 @@ public class FilmController {
     @Autowired
     private FilmService filmService;
 
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(FilmController.class);
+
     @GetMapping
     public ResponseEntity<List<Film>> getAllFilms() {
+        logger.info("Starting operation to retrieve all films");
         List<Film> films = new ArrayList<>();
         try {
-             films = filmService.getAllFilms();
+            films = filmService.getAllFilms();
+            logger.info("Operation completed: {} films retrieved");
         } catch (ResourceNotFoundException e) {
             return new ResponseEntity<>(films, HttpStatus.NOT_FOUND);
         }
@@ -33,16 +38,21 @@ public class FilmController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Film> getFilmById(@PathVariable Long id) {
+        logger.info("Starting operation to retrieve films by ID");
         Film film = filmService.getFilmsById(id);
+        logger.info("Operation completed: film retrieved with ID {}");
         return new ResponseEntity<>(film, HttpStatus.OK);
     }
 
     @PostMapping("/{id}")
     public ResponseEntity<Film> createFilm(@RequestBody Film film) {
         try {
+            logger.info("Starting operation to create a film");
             Film newFilm = filmService.createFilms(film);
+            logger.info("Film creater: ID {}");
             return new ResponseEntity<>(newFilm, HttpStatus.CREATED);
         } catch (InvalidDataException e) {
+            logger.info("Error creating the film: Bad Request");
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
     }
@@ -50,9 +60,12 @@ public class FilmController {
     @PutMapping("/{id}")
     public ResponseEntity<Film> updateFilm(@PathVariable Long id, @RequestBody Film film) {
         try {
+            logger.info("Starting operation to modify a film");
             Film updatedFilm = filmService.updateFilm(id, film);
+            logger.info("Operation completed: film with ID {} has been modified");
             return new ResponseEntity<>(updatedFilm, HttpStatus.OK);
         } catch (ResourceNotFoundException e) {
+            logger.info("Film not found with id {}");
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
@@ -60,9 +73,12 @@ public class FilmController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteList(@PathVariable Long id) {
         try {
+            logger.info("Starting operation to delete a film");
             filmService.deleteFilm(id);
+            logger.info("Operation completed: film deleted with id {}");
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (ResourceNotFoundException e) {
+            logger.info("Film not found with id {}");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
--- api/src/main/java/com/svalero/films/service/FilmService.java
+++ api/src/main/java/com/svalero/films/service/FilmService.java
@@ -3,6 +3,8 @@ package com.svalero.films.service;
 import com.svalero.films.domain.Film;
 import com.svalero.films.exception.ResourceNotFoundException;
 import com.svalero.films.repository.FilmRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,31 +13,38 @@ import java.util.Optional;
 
 @Service
 public class FilmService {
-
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(FilmService.class);
     @Autowired
     private FilmRepository filmRepository;
 
     public List<Film> getAllFilms() {
         try {
+            logger.info("Getting all films from db");
             List<Film> film = filmRepository.findAll();
+            logger.info("{} films retrieved", film.size());
             if (film.isEmpty()) {
+                logger.info("No films found");
                 throw new ResourceNotFoundException("Films not found");
             }
             return film;
         } catch (Exception e) {
+            logger.info("Error while retrieving films");
             throw new ResourceNotFoundException("Error while retrieving films");
         }
     }
 
     public Film getFilmsById(long id) {
+        logger.info("Searching film with ID {}", id);
         Optional<Film> film = filmRepository.findById(id);
         if (film.isEmpty()) {
+            logger.info("Film with ID {} not found", id);
             throw new ResourceNotFoundException("Film not found");
         }
         return film.get();
     }
 
     public Film createFilms(Film film) {
+        logger.info("Creating a new film");
         if (film == null) {
             throw new IllegalArgumentException("Film cannot be null");
         }
@@ -61,6 +70,7 @@ public class FilmService {
     }
 
     public Film updateFilm(Long id, Film film) {
+        logger.info("Updating film with ID {}", id);
         Optional<Film> oldFilm = filmRepository.findById(id);
         if (oldFilm.isEmpty()) {
             throw new ResourceNotFoundException("Film not found");
@@ -86,6 +96,7 @@ public class FilmService {
 
     public void deleteFilm(Long id) {
 
+        logger.info("Deleting film with ID {}", id);
         Film existingFilm = filmRepository.findById(id).orElse(null);
 
         if (existingFilm == null) {

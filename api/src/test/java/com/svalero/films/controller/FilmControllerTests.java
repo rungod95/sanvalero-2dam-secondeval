bebf8355661caf2c27bb3e88589025eb22c053a6
javier <jabierplanos@gmail.com>
2025-05-16 11:28:14 +0200

    Fixed Context Error

--- api/src/test/java/com/svalero/films/controller/FilmControllerTests.java
+++ api/src/test/java/com/svalero/films/controller/FilmControllerTests.java
@@ -1,6 +1,7 @@
 
 package com.svalero.films.controller;
 
+import com.svalero.films.config.TestSecurityConfig;
 import com.svalero.films.domain.Director;
 import com.svalero.films.domain.Film;
 import com.svalero.films.exception.ResourceNotFoundException;
@@ -9,9 +10,15 @@ import com.svalero.films.service.FilmService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,8 +30,8 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(FilmController.class)
+@Import(TestSecurityConfig.class)
 @ActiveProfiles("test")
 class FilmControllerTests {
 
@@ -40,6 +47,9 @@ class FilmControllerTests {
 
     @MockBean
     private FilmService filmService;
+
+    @MockBean
+    private com.svalero.films.repository.FilmRepository filmRepository;
 
 
     @Test
